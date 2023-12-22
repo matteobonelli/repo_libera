0d6be5338e5b9b7ce3497059e52d96241167f1ce
Finnedorn <daniele.giuntoli@outlook.com>
2023-12-22 16:57:20 +0100

    added new caso route on web.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CasoController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+
+Route::get('/caso', [CasoController::class,'index'])->name('caso');
